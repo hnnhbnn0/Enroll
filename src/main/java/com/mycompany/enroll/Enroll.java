e3f2f55c27b1881735ddffccd6bcc96d98dc22ba
Hannah Mae Ciriaco <113029087+hnnhbnn0@users.noreply.github.com>
2023-12-29 14:20:17 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/enroll/Enroll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.enroll;
+
+/**
+ *
+ * @author hnnhc
+ */
+public class Enroll {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
